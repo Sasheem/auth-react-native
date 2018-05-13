2b402de67bbfbc3aeb0a6dced0a3eef78098ff0d
Sasheem <sts12@me.com>
2018-05-13 15:08:20 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class app extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello World from app</Text>
+      </View>
+    );
+  }
+}
+
+export default app;
