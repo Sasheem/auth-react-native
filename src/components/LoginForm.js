b87c49205ce655ff7696ef3c81270f5b96bdae05
Sasheem <sts12@me.com>
2018-05-14 20:47:14 -0400

    made input for password and added secureTextEntry prop

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,7 @@
  import { Button, Card, CardSection, Input } from './common';
 
  class LoginForm extends Component {
-   state = { email: '' };
+   state = { email: '', password: '' };
 
    render() {
      return (
@@ -15,7 +15,15 @@
             onChangeText={email => this.setState({ email })}
           />
         </CardSection>
-        <CardSection />
+        <CardSection>
+          <Input
+            secureTextEntry
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
         <CardSection>
           <Button>Log In</Button>
         </CardSection>
