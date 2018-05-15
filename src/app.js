65c90544ad9540601f845e248e747aed0a70eb45
Sasheem <sts12@me.com>
2018-05-14 20:16:26 -0400

    installed firebase. set up text input component for LoginForm.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class app extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+        apiKey: 'AIzaSyDRPIJ46bcsWmrW5uMimTKLixx2pNVnR_o',
+        authDomain: 'auth-react-native-21dac.firebaseapp.com',
+        databaseURL: 'https://auth-react-native-21dac.firebaseio.com',
+        projectId: 'auth-react-native-21dac',
+        storageBucket: 'auth-react-native-21dac.appspot.com',
+        messagingSenderId: '1046289737056'
+    });
+  }
   render() {
     return (
       <View>
         <Header headerText="Authentication"/>
-        <Text>Hello World from app</Text>
+        <LoginForm />
       </View>
     );
   }
